15f580c11eb594dc06b9ceacc6c86dec0bc204eb
503733196@qq.com <503733196@qq.com>
2018-11-27 14:39:57 +0800

    本地服务配置以及打包配置

--- litemall-all/src/main/java/org/linlinjava/litemall/Application.java
+++ litemall-all/src/main/java/org/linlinjava/litemall/Application.java
@@ -3,18 +3,20 @@ package org.linlinjava.litemall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages = {
-        "org.linlinjava.litemall",
-        "org.linlinjava.litemall.core",
-        "org.linlinjava.litemall.db",
-        "org.linlinjava.litemall.wx",
-        "org.linlinjava.litemall.admin"})
+@SpringBootApplication(scanBasePackages = { "org.linlinjava.litemall", "org.linlinjava.litemall.core",
+		"org.linlinjava.litemall.db", "org.linlinjava.litemall.wx", "org.linlinjava.litemall.admin" })
 @MapperScan("org.linlinjava.litemall.db.dao")
-public class Application {
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
-    }
+public class Application extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
 
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
 }
